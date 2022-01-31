c66b0f06a44a565a929befa624b692ba4ccfe05c
nishant <nishu314@gmail.com>
2022-01-31 11:22:27 +0530

    MUL commit

--- Multiply.java
+++ Multiply.java
@@ -1,5 +1,7 @@
 public class Multiply {
     public static void main(String args[]){
+        System.out.println("Multiplication Operation");
+
         int a= 5;
         int b = 10;
         System.out.println("Sum of numbers:"+a*b);
--- Sum.java
+++
@@ -1,7 +0,0 @@
-public class Sum{
-    public static void main(String args[]){
-        int a= 5;
-        int b = 10;
-        System.out.println("Sum of numbers:"+a+b);
-    }
-}
