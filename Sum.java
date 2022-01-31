efad6daad9954d2076e56ffc2214068fb7b787ca
nishant <nishu314@gmail.com>
2022-01-31 11:28:46 +0530

    Revert "MUL commit"
    This reverts commit c66b0f06a44a565a929befa624b692ba4ccfe05c.

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
-public class Multiply {
+public class Sum{
     public static void main(String args[]){
         int a= 5;
         int b = 10;
-        System.out.println("Sum of numbers:"+a*b);
+        System.out.println("Sum of numbers:"+a+b);
     }
 }
